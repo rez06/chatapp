55c902f7dbbb01bfd917f85aad9cdc70d6d89fe1
rez06 <pelobello.rezyl@gmail.com>
2023-02-10 23:57:14 +0800

    Edited the navbar

--- chatapp-frontend/src/components/Navigation.js
+++ chatapp-frontend/src/components/Navigation.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { LinkContainer } from 'react-router-bootstrap';
 import logo from '../assets/chat-logo.svg';
 
@@ -24,19 +23,6 @@ export default function Navigation() {
             <LinkContainer to ='/chat'>
               <Nav.Link>Chat</Nav.Link>
             </LinkContainer>
-     
-            
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
